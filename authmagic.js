7e7654a575b54c4c07dfd996ddbf7158dac8fd3f
oleks <oleksandrknyga@gmail.com>
2018-05-02 21:24:47 +0300

    proxy plugin

--- authmagic.js
+++ authmagic.js
@@ -7,6 +7,10 @@ module.exports = {
     {
       "name": "authmagic-email-plugin",
       "source": "../authmagic-email-plugin"
+    },
+    {
+      "name": "authmagic-proxy-plugin",
+      "source": "../authmagic-proxy-plugin"
     }
   ],
   "params": {
@@ -28,8 +32,17 @@ module.exports = {
     "authmagic-timerange-stateless-core": {
       "duration": 300,
       "key": "106827392fbe9a1b",
-      "sendKeyPlugin": "authmagic-email-plugin",
+      "sendKeyPlugin": "authmagic-proxy-plugin",
       "expiresIn": 1200
+    },
+    "authmagic-proxy-plugin": {
+      "apiKey": "test_key",
+      "uri": "http://localhost:3001/proxy",
+      "mapItems": [
+        "apiKey",
+        "ekey",
+        "user"
+      ]
     }
   },
   "port": 3000,
